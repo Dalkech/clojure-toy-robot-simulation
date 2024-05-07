ce128cd952255341da4e27b4ce1c0e4ae87d6e35
Julien <julpallec@hotmail.fr>
2024-05-07 17:12:37 +0200

    first commit

---
+++ test/toy_robot_simulation/core_test.clj
@@ -0,0 +1,8 @@
+(ns toy-robot-simulation.core-test
+  (:require [clojure.test :refer :all]
+            [toy-robot-simulation.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 0))))
+
