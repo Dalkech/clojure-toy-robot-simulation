e4211411b6b56523969bafcf436a12e6baf29485
jul <julpallec@hotmail.fr>
2024-05-08 16:41:00 +0200

    place : G

--- src/toy_robot_simulation/commande_manager.clj
+++ src/toy_robot_simulation/commande_manager.clj
@@ -1,4 +1,21 @@
-(ns toy-robot-simulation.commande-manager)
+(ns toy-robot-simulation.commande-manager
+  (:require [clojure.string :as str]))
 
 (defn parse [command]
-  "TODO")
+  (if (str/blank? command)
+    (throw (Exception. (str "wrong command : " command)))
+    ())
+
+  (let [splitted-command (str/split command #" ")]
+      (if (contains? #{"PLACE", "MOVE", "LEFT", "RIGHT", "REPORT"} (first splitted-command))
+
+       (if (and (= "PLACE" (first splitted-command)) (<= 4 (count splitted-command)))
+         {
+          :x (Integer/parseInt (get splitted-command 1)) 
+          :y (Integer/parseInt (get splitted-command 2)) 
+          :direction (get splitted-command 3)
+          }
+         (throw (Exception. (str "wrong command : " command))))
+         
+       (throw (Exception. (str "wrong command : " command)))))
+ )
--- test/toy_robot_simulation/commande_manager_test.clj
+++ test/toy_robot_simulation/commande_manager_test.clj
@@ -3,14 +3,24 @@
              [toy-robot-simulation.commande-manager :refer [parse]]))
 
 
-(deftest ParseCommand
-  (testing "wrong command"
-    ;; (are [command] (thrown? parse command)
-    ;;   "WRONGCOMMAND 5 4 1"
-    ;;   "PLACE 5")
-    ;; )
-    (is (thrown? Throwable (parse "WRONGCOMMAND 5 4 1")) )
-   )
+(deftest ParseWrongCommands
+  (testing "multiple wrong command"
+    (are [wrong-command] (thrown? Throwable (parse wrong-command))
+      "WRONGCOMMAND 5 4 1"
+      ""
+      "PLACE 2"
+      "PLACE 1 2"
+      "PLACE NORTH"
+     )
+    )
   )
 
-  
+(deftest ParseCorrectCommands
+  (are [expected correct-command] (= expected (parse correct-command))
+    {:x 1 :y 2 :direction "NORTH"} "PLACE 1 2 NORTH"
+    {:x 5 :y 5 :direction "SOUTH"} "PLACE 5 5 SOUTH"
+    {:x 4 :y 4 :direction "EAST"} "PLACE 4 4 EAST"
+    {:x 3 :y 3 :direction "WEST"} "PLACE 3 3 WEST"
+    {:x -1 :y 3 :direction "WEST"} "PLACE -1 3 WEST"
+    )
+  )
